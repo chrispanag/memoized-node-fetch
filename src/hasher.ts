e88eb499e3643c2e7c9acca8db3c1b403bbe86ce
Christos Panagiotakopoulos <chrispanag@gmail.com>
2020-09-28 15:13:02 +0300

    Run prettier

--- src/hasher.ts
+++ src/hasher.ts
@@ -7,7 +7,7 @@ export default function stringToHash(string: string) {
 
     for (let i = 0; i < string.length; i++) {
         const char = string.charCodeAt(i);
-        hash = ((hash << 5) - hash) + char;
+        hash = (hash << 5) - hash + char;
         hash = hash & hash;
     }
 
