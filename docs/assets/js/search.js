d30b70eb22a2aa843c79d38b0311bde09ab23635
Christos Panagiotakopoulos <chrispanag@gmail.com>
2020-09-29 11:05:25 +0300

    Fix docs

---
+++ docs/assets/js/search.js
@@ -0,0 +1,3 @@
+var typedoc = typedoc || {};
+            typedoc.search = typedoc.search || {};
+            typedoc.search.data = {"kinds":{"64":"Function","65536":"Type literal","4194304":"Type alias"},"rows":[{"id":0,"kind":64,"name":"memoizedNodeFetchFactory","url":"globals.html#memoizednodefetchfactory","classes":"tsd-kind-function"},{"id":1,"kind":4194304,"name":"FetchFunctionType","url":"globals.html#fetchfunctiontype","classes":"tsd-kind-type-alias"},{"id":2,"kind":65536,"name":"__type","url":"globals.html#fetchfunctiontype.__type","classes":"tsd-kind-type-literal tsd-parent-kind-type-alias","parent":"FetchFunctionType"}]};
